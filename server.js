a544c5bbd616346e1dcdcac1f225efb7f2324d6b
shama2025 <101298098+shama2025@users.noreply.github.com>
2022-03-24 16:02:18 -0400

    Update server.js

--- server.js
+++ server.js
@@ -72,3 +72,152 @@ app.get("/viewData", function(req,res){
 
 // start the server to listen on HTTP_PORT
 app.listen(HTTP_PORT, onHttpStart);
+/*Mongoose code)
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+// connect to Your MongoDB Atlas Database
+
+mongoose.connect("Input connection string here");
+
+// define the company schema
+var employSchema = new Schema({
+  "Name": {
+    type: String,
+    unique: true
+  },
+  "Age": {
+    type: Number,
+    default: 0
+  },
+  "Occupation": String,
+  "Company": String
+});
+
+var employee = mongoose.model("web322_employees", employSchema);
+
+// create a new company
+var employee1 = new employee({
+  Name: "John",
+  Age: 20,
+  Occupation: "Developer",
+  Company: "SocotiaBank"
+});
+// define the company schema
+
+var workSchema = new Schema({
+    "Name": {
+        type: String,
+        unique: true
+      },
+    "Age": {
+      type: Number,
+      default: 0
+    },
+    "Occupation": String,
+    "Company": String
+});
+var Work = employee;
+var employee2 = new Work({
+    Name: "Sarah",
+    Age: 32,
+    Occupation: "Manager",
+    Company: "TD"
+});
+var employee3Schema = new Schema({
+    "Name": {
+        type: String,
+        unique: true
+      },
+    "Age": {
+      type: Number,
+      default: 0
+    },
+    "Occupation": String,
+    "Company": String
+});
+var Business = Work;
+var employee3 = new Business({
+    Name: "Charles",
+    Age: 28,
+    Occupation: "Programmer",
+    Company: "IUK Web Team"
+});
+
+
+employee.deleteMany({Name: "John"})
+  .exec()
+  .then(() => {
+      console.log("removed employee");
+  })
+  .catch((err) => {
+      console.log(err);
+});
+// save the company
+employee1.save((err) => {
+  if(err) {
+    console.log(`There was an error saving the employee 1 information: ${err}`);
+  } else {
+    console.log("The employee was saved to the web322_companies collection");
+  }
+  employee.find({ Name: "John" })
+  .exec()
+  .then((employee1) => {
+    if(!employee1) {
+      console.log("No employee was found");
+    } else {
+      console.log(employee1);
+    }
+    // exit the program after saving
+    process.exit();
+  })
+  .catch((err) => {
+    console.log(`There was an error: ${err}`);
+  });
+});
+
+
+employee2.save((err) => {
+  if(err) {
+    console.log(`There was an error saving the employee 2 information: ${err}`);
+  } else {
+    console.log("Walmart company was saved to the web322_companies collection");
+  }
+
+  Work.find({ Name: "Sarah" })
+  .exec()
+  .then((employee2) => {
+    if(!employee2) {
+      console.log("No employee was found");
+    } else {
+      console.log(employee2);
+    }
+    // exit the program after saving
+    process.exit();
+  })
+  .catch((err) => {
+    console.log(`There was an error: ${err}`);
+  });
+});
+
+employee3.save((err) => {
+  if(err) {
+    console.log(`There was an error saving the employee 3 information: ${err}`);
+  } else {
+    console.log("The employee information was saved to the web322_companies collection");
+  }
+  Business.find({ Name: "Charles" })
+  .exec()
+  .then((employee3) => {
+    if(!employee3) {
+      console.log("No employee was found");
+    } else {
+      console.log(employee3);
+    }
+    // exit the program after saving
+    process.exit();
+  })
+  .catch((err) => {
+    console.log(`There was an error: ${err}`);
+  });
+});
+*/
